05f52fe772456c234115109ce87b554923273ec6
Akhil Narang <me@akhilnarang.dev>
2021-06-02 12:16:35 +0530

    Update PowerHAL from LA.UM.9.1.r1-06700-SMxxx0.0
    Thanks to Abhishek for helping with this
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- power/power-vendor-product.mk
+++ power/power-vendor-product.mk
@@ -1,6 +1,6 @@
 #Power product definitions
-PRODUCT_PACKAGES += android.hardware.power@1.2-impl
-PRODUCT_PACKAGES += android.hardware.power@1.2-service
+PRODUCT_PACKAGES += android.hardware.power-service
+PRODUCT_PACKAGES += android.hardware.power-impl
 
 #Powerhint File
 ifeq ($(TARGET_BOARD_PLATFORM),msmnile)
@@ -11,6 +11,8 @@ else ifeq ($(TARGET_BOARD_PLATFORM),lito)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/lito/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),atoll)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/atoll/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),sm6150)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/sm6150/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),bengal)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/bengal/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
